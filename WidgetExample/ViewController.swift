ed6925dcff6d2d1dce8d2f4b9d25d5ae1a3127da
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2022-10-12 16:29:11 +0200

    Initial Commit

---
+++ WidgetExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WidgetExample
+//
+//  Created by Kaori Persson on 2022-10-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
