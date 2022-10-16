c3c443725285fed79f8aa5dc33e1a03c0f1a58d0
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2022-10-16 11:04:23 +0200

    Add: textfield and button

--- WidgetExample/ViewController.swift
+++ WidgetExample/ViewController.swift
@@ -8,10 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+	private let field: UITextField = {
+		let field = UITextField()
+		field.placeholder = "Enter text"
+		field.backgroundColor = .white
+		return field
+	}()
+	
+	private let button: UIButton = {
+		let button = UIButton()
+		button.setTitleColor(.white, for: .normal)
+		button.setTitle("Save", for: .normal)
+		return button
+	}()
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		
+		view.backgroundColor = .systemBlue
+		view.addSubview(field)
+		view.addSubview(button)
+		field.becomeFirstResponder()
+		
+		button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
 	}
 
 
